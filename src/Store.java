5e05075fa0d1246c1eebf83b8e49fdd8ac63ed3d
anthonydelsignore <adelsignore@student.bridgew.efu>
2021-10-04 17:09:21 -0400

    project 2

---
+++ src/Store.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Store {
+    private ArrayList<Order>;
+    private ArrayList<Customer>;
+    public main(args:string{});
+    public runStore();
+    public makeOrder(address: ShippingAdress, cust:Customer);
+    public Store();
+    public addCustomer(): Customer;
+    public selectCustomer(); Customer;
+    public manageCustomer(selectedCustomer: Customer);
+}
